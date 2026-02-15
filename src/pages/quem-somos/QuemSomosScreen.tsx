166450d8e2396e3b8be650989a8faacee153dba3
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-02-15 16:25:54 -0300

    fix: adjust text size for improved readability in Quem Somos screen

--- src/pages/quem-somos/QuemSomosScreen.tsx
+++ src/pages/quem-somos/QuemSomosScreen.tsx
@@ -7,12 +7,12 @@ export default function QuemSomosScreen() {
         <div>
             <SobreNos />
 
-            <section className="w-full bg-[#03ABB6] py-16 px-6 text-center">
-                <div className="max-w-4xl mx-auto">
-                    <p className="text-white text-lg md:text-[22px] font-semibold leading-relaxed">
+            <section className="w-full bg-[#03ABB6] py-16 px-6  text-center">
+                <div className="mx-auto">
+                    <p className="text-white text-lg md:text-2xl font-semibold leading-relaxed">
                         Já ajudamos centenas de pessoas a reencontrar o prazer de se alimentar e viver com dignidade.
                     </p>
-                    <p className="text-white text-lg md:text-[22px] font-semibold leading-relaxed mt-0">
+                    <p className="text-white text-lg md:text-2xl font-semibold leading-relaxed mt-0">
                         Porque viver com Acalásia pode ser difícil, mas juntos, fica mais leve.
                     </p>
                 </div>
