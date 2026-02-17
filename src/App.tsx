cd1c3e1918ab76e35c086647256187fc504b157d
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-02-16 21:50:27 -0300

    feat: add WhatsappButton component for direct contact via WhatsApp

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import Menu from "./componentes/menu/Menu";
 import { Rodape } from "./componentes/rodape/Rodape";
 import { ScrollToTop } from "./componentes/scroll-to-top/ScrollToTop";
+import WhatsappButton from "./componentes/whatsapp-button/WhatsappButton";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Menu />
       <div className="h-[100px] sm:h-[180px] md:h-[200px]"></div>
       <Outlet />
+      <WhatsappButton />
       <Rodape />
     </>
   );
---
+++ src/componentes/whatsapp-button/WhatsappButton.tsx
@@ -0,0 +1,35 @@
+import { FaWhatsapp } from "react-icons/fa";
+
+export default function WhatsappButton() {
+  const numero = "5581992479363"; // coloque seu número com DDI
+  const link = `https://wa.me/${numero}`;
+
+  return (
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="fixed left-10 bottom-18 z-50 group flex items-center"
+    >
+
+      {/* Botão */}
+      <div className="w-16 h-16 flex items-center justify-center 
+        rounded-full bg-green-500 text-white text-2xl shadow-lg
+        transform transition duration-300
+        group-hover:scale-110"
+      >
+        <FaWhatsapp size={35} />
+      </div>
+
+      {/* Texto */}
+      <span className="inline-block 
+        bg-black text-white text-md px-4 py-1 rounded-full
+        translate-x-1
+        group-hover:opacity-100 group-hover:translate-x-2
+        transition-all duration-300"
+      >
+        Whatsapp
+      </span>
+    </a>
+  );
+}
