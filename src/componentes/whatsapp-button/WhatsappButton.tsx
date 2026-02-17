c556a5fa48eef47fc317348c6b4434e4333fc6d0
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-02-16 21:53:57 -0300

    fix: adjust positioning and sizing of WhatsappButton component for better visibility

--- src/componentes/whatsapp-button/WhatsappButton.tsx
+++ src/componentes/whatsapp-button/WhatsappButton.tsx
@@ -9,11 +9,11 @@ export default function WhatsappButton() {
       href={link}
       target="_blank"
       rel="noopener noreferrer"
-      className="fixed left-10 bottom-18 z-50 group flex items-center"
+      className="fixed left-4 md:left-10 bottom-4 md:bottom-18 z-50 group flex items-center"
     >
 
       {/* Botão */}
-      <div className="w-16 h-16 flex items-center justify-center 
+      <div className="w-12 h-12 md:w-16 md:h-16 flex items-center justify-center 
         rounded-full bg-green-500 text-white text-2xl shadow-lg
         transform transition duration-300
         group-hover:scale-110"
@@ -23,7 +23,7 @@ export default function WhatsappButton() {
 
       {/* Texto */}
       <span className="inline-block 
-        bg-black text-white text-md px-4 py-1 rounded-full
+        bg-black text-white text-sm md:text-md px-4 py-1 rounded-full
         translate-x-1
         group-hover:opacity-100 group-hover:translate-x-2
         transition-all duration-300"
