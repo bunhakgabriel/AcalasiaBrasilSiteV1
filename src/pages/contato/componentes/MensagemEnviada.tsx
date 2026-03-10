96efa7a38578ce2007196c702249bfd751e45f86
Gabriel Bunhak <bunhakgabriel@gmail.com>
2026-03-10 16:53:44 -0300

    corrige bug visual

--- src/pages/contato/componentes/MensagemEnviada.tsx
+++ src/pages/contato/componentes/MensagemEnviada.tsx
@@ -29,7 +29,7 @@ function MensagemEnviada() {
             <div className="flex flex-col justify-center bg-white max-w-xl w-full rounded-xl shadow-lg p-8 sm:p-10 text-center">
 
                 <div className="flex justify-center mb-6">
-                    <FaCheckCircle className="text-teal-500 text-6xl sm:text-7xl" />
+                    <FaCheckCircle className="text-[#03ABB6] text-6xl sm:text-7xl" />
                 </div>
 
                 <h1 className="text-2xl sm:text-3xl font-semibold text-gray-800 mb-4">
@@ -42,10 +42,8 @@ function MensagemEnviada() {
                 </p>
 
                 <p className="text-gray-500 mb-8 text-sm sm:text-base">
-                    Você será redirecionado para a página inicial em{" "}
-                    <span className="font-semibold text-teal-600">
-                        {tempo}
-                    </span>{" "}
+                    Você será redirecionado para a página inicial em
+                    <span className="font-semibold text-teal-600"> {tempo} </span>
                     segundos.
                 </p>
 
@@ -53,14 +51,14 @@ function MensagemEnviada() {
 
                     <Link
                         to="/"
-                        className="bg-teal-500 hover:bg-teal-600 text-white px-6 py-3 rounded-lg transition"
+                        className="bg-[#03ABB6] hover:bg-teal-600 text-white px-6 py-3 rounded-lg transition"
                     >
                         Voltar para o início
                     </Link>
 
                     <Link
                         to="/contato"
-                        className="border border-teal-500 text-teal-500 hover:bg-teal-50 px-6 py-3 rounded-lg transition"
+                        className="border border-teal-500 text-[#03ABB6] hover:bg-teal-50 px-6 py-3 rounded-lg transition"
                     >
                         Enviar outra mensagem
                     </Link>
